b98fa191be6cdb1f05a16521b8cc811b87decafd
Leon Ormes <leonormes@gmail.com>
2017-11-18 07:05:22 +0000

    Move logic

---
+++ src/components/phonicSchemeList/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+
+const phonicSchemesList = ({data: {phonicSchemes = []}}) =>
+  phonicSchemes.map((ps) => (
+    <div key={ps.id}>
+      <h2>{ps.name}</h2>
+      <p>{ps.description}</p>
+    </div>
+  ));
+
+const phonicSchemeListQuery = gql`
+  query PhonicsSchemeQuery {
+    phonicSchemes {
+      id
+      name
+      description
+    }
+  }
+`;
+
+export default graphql(phonicSchemeListQuery)(phonicSchemesList);
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import gql from 'graphql-tag';
-import {graphql} from 'react-apollo';
 
-const Home = ({data: {phonicSchemes = []}}) =>
-  phonicSchemes.map((ps) => (
-    <div key={ps.id}>
-      <h2>{ps.name}</h2>
-      <p>{ps.description}</p>
+import Phonicschemelist from '../components/phonicSchemeList';
+
+const Home = () => {
+  return (
+    <div>
+      <Phonicschemelist />
     </div>
-  ));
+  );
+};
 
-const phonicSchemeListQuery = gql`
-  query PhonicsSchemeQuery {
-    phonicSchemes {
-      id
-      name
-      description
-    }
-  }
-`;
-
-export default graphql(phonicSchemeListQuery)(Home);
+export default Home;
